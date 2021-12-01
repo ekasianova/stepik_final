3db6ac0e8d48ddb83242534c11f182cb9c541602
ekasianova <88433318+ekasianova@users.noreply.github.com>
2021-12-01 16:26:09 +0300

    Refactored base_page.py

--- pages/base_page.py
+++ pages/base_page.py
@@ -13,18 +13,11 @@ class BasePage():
     def __init__(self, browser, url, timeout=10):
         self.browser = browser
         self.url = url
-        self.browser.implicitly_wait(timeout)
-        
+        self.browser.implicitly_wait(timeout) 
         
     def open(self):
         self.browser.get(self.url)
-        
-    def is_element_present(self, how, what):
-        try:
-            self.browser.find_element(how, what)
-        except NoSuchElementException:
-            return False
-        return True
+    
     def solve_quiz_and_get_code(self):
         alert = self.browser.switch_to.alert
         x = alert.text.split(" ")[2]
@@ -38,6 +31,13 @@ class BasePage():
             alert.accept()
         except NoAlertPresentException:
             print("No second alert presented")
+            
+    def is_element_present(self, how, what):
+        try:
+            self.browser.find_element(how, what)
+        except NoSuchElementException:
+            return False
+        return True
 
     def is_not_element_present(self, how, what, timeout=4):
         try:
